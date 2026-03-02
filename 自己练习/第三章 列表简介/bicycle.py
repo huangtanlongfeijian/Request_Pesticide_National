ce56ce50684271c24bc33b8148a8d982c921217a
huangtanlongfeijian <longjianhuang1994@outlook.com>
2026-03-02 22:56:00 +0800

    update

---
+++ 自己练习/第三章 列表简介/bicycle.py
@@ -0,0 +1,8 @@
+name = ["Eva","Elsa","Annie","Nino","Grace","Joyce"]
+
+print(name[0])
+print(name[0].lower())
+
+message = f"My Name is {name[0].title()} , Good morning!"
+
+print(message)
---
+++ 自己练习/第二章 变量与简单数据类型/number.py
@@ -0,0 +1,8 @@
+print(4+4)
+print(9-1)
+print(2*4)
+print(f"{16/2:.0f}")
+
+
+best_forvalue = 4
+print(f"你最喜欢的数字是{best_forvalue:.1f}")
